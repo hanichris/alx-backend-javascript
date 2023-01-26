f9da0078017ce32e32b98e9f4b275295b60d7acd
hanichris <hanichris71@gmail.com>
2023-01-26 23:39:11 +0300

    log function response

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -2,6 +2,6 @@ import { uploadPhoto, createUser } from './utils';
 
 export default function handleProfileSignup() {
   return Promise.all(uploadPhoto(), createUser())
-    .then((res) => console.log(`Response: ${res}`))
+    .then((res) => console.log(res))
     .catch(() => console.log('Signup system offline'));
 }
