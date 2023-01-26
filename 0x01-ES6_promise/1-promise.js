2a0b06128ae8d340005193cf3ae850f9049804b5
root <hanichris@gmail.com>
2023-01-26 19:50:55 +0000

    fix bug in 1-promise.js

--- 0x01-ES6_promise/1-promise.js
+++ 0x01-ES6_promise/1-promise.js
@@ -1,7 +1,10 @@
 export default function getFullResponseFromAPI(success) {
   return new Promise((resolve, reject) => {
     if (success) {
-      resolve({ status: 200, body: 'Sucess', });
+      resolve({
+        status: 200,
+        body: 'Sucess',
+      });
     } else {
       reject(new Error('The fake API is not working currently'));
     }
