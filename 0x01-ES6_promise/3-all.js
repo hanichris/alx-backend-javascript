79e5875320e0d2dbeafd85c4d609c226ce61ba7e
hanichris <hanichris71@gmail.com>
2023-01-26 23:32:45 +0300

    Return a promise object from the file.

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,7 +1,7 @@
 import { uploadPhoto, createUser } from './utils';
 
 export default function handleProfileSignup() {
-  Promise.all([uploadPhoto, createUser])
-    .then(() => console.log(`${createUser.firstName} ${createUser.lastName}`))
-    .catch(() => console.log('Signup system offline'));
+  return Promise.all(uploadPhoto(), createUser())
+    .then((res) => console.log(`Response: ${res}`))
+    .catch(() => console.log("Signup system offline"));
 }
