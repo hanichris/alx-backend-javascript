3bf0c9e8c61ee93c6a86b24d0e6c81faf37e91f2
root <hanichris@gmail.com>
2023-01-26 20:35:44 +0000

    change

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -3,5 +3,5 @@ import { uploadPhoto, createUser } from './utils';
 export default function handleProfileSignup() {
   return Promise.all(uploadPhoto(), createUser())
     .then((res) => console.log(`Response: ${res}`))
-    .catch(() => console.log("Signup system offline"));
+    .catch(() => console.log('Signup system offline'));
 }
