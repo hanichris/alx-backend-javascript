0a16ff202e1e434b6037f039cb343e9be65fc3af
hanichris <hanichris71@gmail.com>
2023-01-27 00:32:38 +0300

    eslint error fix

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -5,6 +5,6 @@ export default function handleProfileSignup(firstName, lastName, fileName) {
   return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
     .then((res) => res.map((obj) => ({
       status: obj.status,
-      value: obj.status === 'fulfilled' ? obj.value: String(obj.reason),
+      value: obj.status === 'fulfilled' ? obj.value : String(obj.reason),
     })));
 }
