03ce47465868dbfe0c32fec8ae4f45741a393699
hanichris <hanichris71@gmail.com>
2023-01-27 19:12:40 +0300

    object access using square bracket notation

--- 0x01-ES6_promise/100-await.js
+++ 0x01-ES6_promise/100-await.js
@@ -1,15 +1,15 @@
-import {uploadPhoto, createUser} from './utils'
+import { uploadPhoto, createUser } from './utils';
 
 export default async function asyncUploadUser() {
   const output = {};
   try {
     const res = await uploadPhoto();
     const res1 = await createUser();
-    output[photo] = res;
-    output[user] = res1; 
+    output['photo'] = res;
+    output['user'] = res1;
   } catch (error) {
-    output[photo] = null;
-    output[user] = null;
+    output['photo'] = null;
+    output['user'] = null;
   }
 
   return output;
