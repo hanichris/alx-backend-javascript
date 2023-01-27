f3c6445329bbaa88227ed3b9a3ef04f61e84c2ee
hanichris <hanichris71@gmail.com>
2023-01-27 19:09:38 +0300

    Using async and await

---
+++ 0x01-ES6_promise/100-await.js
@@ -0,0 +1,16 @@
+import {uploadPhoto, createUser} from './utils'
+
+export default async function asyncUploadUser() {
+  const output = {};
+  try {
+    const res = await uploadPhoto();
+    const res1 = await createUser();
+    output[photo] = res;
+    output[user] = res1; 
+  } catch (error) {
+    output[photo] = null;
+    output[user] = null;
+  }
+
+  return output;
+}
