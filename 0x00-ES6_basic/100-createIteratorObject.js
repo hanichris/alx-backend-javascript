d95ba8e70b04d22974c458191f7e93d7578e38e3
hanichris <hanichris71@gmail.com>
2023-01-28 06:01:01 +0300

    esiint correction

--- 0x00-ES6_basic/100-createIteratorObject.js
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -1,9 +1,9 @@
 export default function createIteratorObject(report) {
-  return function* () {
+  return (function* () {
     for (const dept of Object.values(report.allEmployees)) {
       for (const emp of dept) {
         yield emp;
       }
     }
-  }();
+  }());
 }
