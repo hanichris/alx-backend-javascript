faf855c3939607d8fa37ae23114f1b978958a4eb
hanichris <hanichris71@gmail.com>
2023-01-31 19:14:22 +0300

    change conflicting variable names

--- 0x03-ES6_data_manipulation/9-groceries_list.js
+++ 0x03-ES6_data_manipulation/9-groceries_list.js
@@ -1,11 +1,11 @@
 const groceriesList = () => {
   const myMap = new Map();
 
-  map.set('Apples', 10);
-  map.set('Tomatoes', 10);
-  map.set('Pasta', 1);
-  map.set('Rice', 1);
-  map.set('Banana', 5);
+  myMap.set('Apples', 10);
+  myMap.set('Tomatoes', 10);
+  myMap.set('Pasta', 1);
+  myMap.set('Rice', 1);
+  myMap.set('Banana', 5);
 
   return myMap;
 };
