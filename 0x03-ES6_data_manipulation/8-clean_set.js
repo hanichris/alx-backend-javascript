db7cbc542834636409823a87c8d35703af27f3d3
hanichris <hanichris71@gmail.com>
2023-01-31 18:21:30 +0300

    Move to appropriate folder

---
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -0,0 +1,13 @@
+export default function cleanSet(testSet, startString) {
+  if (!startString) {
+    return;
+  }
+
+  res = [];
+  for (const element of testSet) {
+    if(element.match(startString)){
+      res.push(element.substring(startString.length));
+    }
+  }
+  return res.join('-');
+}
