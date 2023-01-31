35d0927e648af01a26953826f2f97148d68b62dc
hanichris <hanichris71@gmail.com>
2023-01-31 20:05:38 +0300

    greater than 4

--- 0x03-ES6_data_manipulation/100-weak.js
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -2,7 +2,7 @@ export const weakMap = new WeakMap();
 
 export const queryAPI = (endpoint) => {
   const count = weakMap.get(endpoint) || 0;
-  if (count >= 5) {
+  if (count >= 4) {
     throw new Error('Endpoint load is high');
   }
   weakMap.set(endpoint, count + 1);
