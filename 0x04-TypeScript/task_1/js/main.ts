bef1b92aba3e34adbee0b0c2fafcbf8ab3d8d0cb
hanichris <hanichris71@gmail.com>
2023-02-02 03:14:16 +0300

    Teacher interface.

---
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -0,0 +1,12 @@
+// First two attributes cannot be re-written to after initialization.
+// Use `readonly` to achieve this.
+// Can add an attribute of any type to the object with this interface without
+// specifying what the name will be.
+interface Teacher {
+    readonly firstName: string;
+    readonly lastName: string;
+    fullTimeEmployee: boolean;
+    yearsOfExperience?: number;
+    location: string;
+    [propName: string]: any;
+}
