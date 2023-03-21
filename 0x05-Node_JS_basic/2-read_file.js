404b5e8269cd3e2175e98a1dd5ffa04948b35b4c
hanichris <hanichris71@gmail.com>
2023-03-21 23:27:38 +0300

    fix eslint errors.

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -5,14 +5,14 @@ function countStudents(path) {
   if (!fs.existsSync(path) || !fs.statSync(path).isFile()) {
     throw new Error('Cannot load the database');
   }
-  
+
   const stream = fs.createReadStream(path);
-  const rl = readline.createInterface({input: stream});
+  const rl = readline.createInterface({ input: stream });
   const studentsByField = {};
   let count = 0;
 
   rl.on('line', (line) => {
-    if (line !== "firstname,lastname,age,field" && line !== '') {
+    if (line !== 'firstname,lastname,age,field' && line !== '') {
       count += 1;
       const row = line.split(',');
       const firstName = row[0];
@@ -27,11 +27,10 @@ function countStudents(path) {
   rl.on('close', () => {
     console.log(`Number of students: ${count}`);
     for (const [key, value] of Object.entries(studentsByField)) {
-      string_value = value.join(', ');
-      console.log(`Number of students in ${key}: ${value.length}. List: ${string_value}`);
+      const stringValue = value.join(', ');
+      console.log(`Number of students in ${key}: ${value.length}. List: ${stringValue}`);
     }
-  })
-
+  });
 }
 
 module.exports = countStudents;
