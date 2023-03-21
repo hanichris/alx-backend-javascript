1ff26d4d921ee01a6634ec2ef6c5a0735b59798e
hanichris <hanichris71@gmail.com>
2023-03-21 17:00:41 +0300

    use .toString() method

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -6,7 +6,7 @@ const rl = readline.createInterface({
 });
 
 rl.question('Welcome to Holberton School, what is your name?\n', (answer) => {
-  process.stdout.write(`Your name is: ${answer}`);
+  process.stdout.write(`Your name is: ${answer.toString()}`);
   if (process.stdin.isTTY) {
     process.exit();
   }
