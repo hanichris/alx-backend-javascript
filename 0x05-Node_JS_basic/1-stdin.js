37c889fb2dc3b7b96db2a9351fb4b833c0b28feb
hanichris <hanichris71@gmail.com>
2023-03-22 16:17:07 +0300

    Placing an `end` listener on readline.

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,6 +1,6 @@
 process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
-process.stdin.on('data', () => {
+process.stdin.on('readable', () => {
   const chunk = process.stdin.read();
 
   if (chunk) {
