b2d9879f91421d4618d32198ba15da14896d220b
hanichris <hanichris71@gmail.com>
2023-03-22 15:46:16 +0300

    Using process.stdout.write to print out to stdout

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,17 +1,1 @@
-const readline = require('readline');
-
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
-});
-
-rl.question('Welcome to Holberton School, what is your name?\n', (answer) => {
-  console.log(`Your name is: ${answer}`);
-  rl.close()
-});
-
-rl.on('close', () => {
-  if (!process.stdin.isTTY) {
-    console.log('This important software is now closing');
-  }
-});
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
