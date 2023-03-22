32cddaa45ce08d039cd58a9b48a05c0e0954c6d5
hanichris <hanichris71@gmail.com>
2023-03-22 15:58:03 +0300

    Test if module is run in __main__

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,1 +1,9 @@
 process.stdout.write('Welcome to Holberton School, what is your name?\n');
+
+if (process.stdin.isTTY) {
+  process.stdin.on('data', (data) => {
+    console.log(`Your name is: ${data.toString()}`);
+  })
+} else {
+  
+}
