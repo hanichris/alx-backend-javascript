90e52cebad2f24d05f1fee070cd43d04f0cf788a
hanichris <hanichris71@gmail.com>
2023-03-24 16:53:04 +0300

    Read a file asynchronously catching any error during the process.

---
+++ 0x05-Node_JS_basic/3-read_file_async.js
@@ -0,0 +1,12 @@
+const fs = require('fs');
+
+function countStudents(path) {
+  fs.readFile(path, 'utf8', (err, data) => {
+    if (err) {
+      throw new Error('Cannot load the database');
+    }
+    console.log(data);
+  })
+}
+
+module.exports = countStudents;
