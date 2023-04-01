5ab8b522946b55396ff0466a9cbc026a69e325da
hanichris <hanichris71@gmail.com>
2023-04-01 21:57:41 +0300

    More complex HTTP server.
    Manages routes througn the http request object.

---
+++ 0x05-Node_JS_basic/5-http.js
@@ -0,0 +1,58 @@
+const http = require('http');
+
+function countStudents(path) {
+  return new Promise((resolve, reject) => {
+    fs.readFile(path, 'utf8', (err, data) => {
+      if (err) {
+        reject(new Error('Cannot load the database'));
+      } else {
+        const studentsByField = {};
+        let data = '';
+        let studentCount = 0;
+        const preProcessedData = data.trim().split('\n');
+        for (const line of preProcessedData.slice(1)) {
+          studentCount += 1;
+          const row = line.split(',');
+          const [firstName, field] = [row[0], row[row.length - 1]];
+          if (!studentsByField[field]) {
+            studentsByField[field] = [];
+          }
+          studentsByField[field].push(firstName);
+        }
+        data += `Number of students: ${studentCount}\n`;
+        for (const [key, value] of Object.entries(studentsByField)) {
+          const sValue = value.join(', ');
+          data += `Number of students in ${key}: ${value.length}. List: ${sValue}\n`;
+        }
+        resolve(data);
+      }
+    });
+  });
+}
+
+const host = 'localhost';
+const port = 1245;
+
+const app = http.createServer((req, res) => {
+  res.setHeader('Content-Type', 'text/plain');
+  switch (req.url) {
+    case '/students':
+      res.statusCode = 200;
+      res.write('This is the list of our students\n');
+      countStudents(process.argv[2]).then((data) => {
+        res.end(data);
+      }).catch((err) => {
+        res.statusCode = 404;
+        res.end(`${err}`);
+      });
+      break;
+
+    default:
+      res.writeHead(200);
+      res.end('Hello Holberton School!');
+      break;
+  }
+});
+
+app.listen(port, host);
+module.exports = app;
