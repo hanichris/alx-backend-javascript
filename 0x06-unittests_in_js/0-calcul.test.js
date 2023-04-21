323d985c861d399a8a93f91899918fef178795dd
hanichris <hanichris71@gmail.com>
2023-04-22 02:22:13 +0300

    Change imports from ES6 to CommonJS format

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -1,29 +1,29 @@
-import { assert } from 'assert';
-import { describe, it } from 'mocha';
-import calculateNumber from './0-calcul';
+const assert = require('assert');
+const { describe, it } = require('mocha');
+const calculateNumber = require('./0-calcul');
 
 describe('calculateNumber', function () {
   it('correct sum when given whole integers', function () {
-    assert.strictEqual(calculateNumber(1, 3), 4);
+    assert.assert.strictEqual(calculateNumber(1, 3), 4);
   });
 
   it('correct sum when first argument has a decimal part less than .5', function () {
-    assert.strictEqual(calculateNumber(1.2, 3), 4);
+    assert.assert.strictEqual(calculateNumber(1.2, 3), 4);
   });
 
   it('correct sum when first argument has a decimal part greater than .5', function () {
-    assert.strictEqual(calculateNumber(1.6, 3), 5);
+    assert.assert.strictEqual(calculateNumber(1.6, 3), 5);
   });
 
   it('correct sum when second argument has a decimal part less than .5', function () {
-    assert.strictEqual(calculateNumber(1, 3.2), 4);
+    assert.assert.strictEqual(calculateNumber(1, 3.2), 4);
   });
 
   it('correct sum when second argument has a decimal part greater than .5', function () {
-    assert.strictEqual(calculateNumber(1, 3.6), 5);
+    assert.assert.strictEqual(calculateNumber(1, 3.6), 5);
   });
 
   it('correct sum when both arguments are decimals', function () {
-    assert.strictEqual(calculateNumber(1.2, 3.6), 5);
+    assert.assert.strictEqual(calculateNumber(1.2, 3.6), 5);
   });
 });
