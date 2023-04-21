04f19a0a3544108e2db0041b05da819baaf34991
hanichris <hanichris71@gmail.com>
2023-04-22 02:24:23 +0300

    update call to .strictEqual of assert module.

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -4,26 +4,26 @@ const calculateNumber = require('./0-calcul');
 
 describe('calculateNumber', function () {
   it('correct sum when given whole integers', function () {
-    assert.assert.strictEqual(calculateNumber(1, 3), 4);
+    assert.strictEqual(calculateNumber(1, 3), 4);
   });
 
   it('correct sum when first argument has a decimal part less than .5', function () {
-    assert.assert.strictEqual(calculateNumber(1.2, 3), 4);
+    assert.strictEqual(calculateNumber(1.2, 3), 4);
   });
 
   it('correct sum when first argument has a decimal part greater than .5', function () {
-    assert.assert.strictEqual(calculateNumber(1.6, 3), 5);
+    assert.strictEqual(calculateNumber(1.6, 3), 5);
   });
 
   it('correct sum when second argument has a decimal part less than .5', function () {
-    assert.assert.strictEqual(calculateNumber(1, 3.2), 4);
+    assert.strictEqual(calculateNumber(1, 3.2), 4);
   });
 
   it('correct sum when second argument has a decimal part greater than .5', function () {
-    assert.assert.strictEqual(calculateNumber(1, 3.6), 5);
+    assert.strictEqual(calculateNumber(1, 3.6), 5);
   });
 
   it('correct sum when both arguments are decimals', function () {
-    assert.assert.strictEqual(calculateNumber(1.2, 3.6), 5);
+    assert.strictEqual(calculateNumber(1.2, 3.6), 5);
   });
 });
