3641a5561a0e1523250991ac9bd9a9784b963cee
hanichris <hanichris71@gmail.com>
2023-04-22 03:26:23 +0300

    use object destructing to import expect module.

--- 0x06-unittests_in_js/2-calcul.test.js
+++ 0x06-unittests_in_js/2-calcul.test.js
@@ -1,4 +1,4 @@
-const expect = require('chai').expect;
+const { expect } = require('chai');
 const { describe, it } = require('mocha');
 const calculateNumber = require('./1-calcul');
 
@@ -57,27 +57,27 @@ describe('calculateNumber', function () {
 
   describe('DIVIDE functionality', function () {
     it('correct division when given whole integers', function () {
-      expect(calculateNumber('DIVIDE', 1, 3)).to.equal(1/3);
+      expect(calculateNumber('DIVIDE', 1, 3)).to.equal(1 / 3);
     });
   
     it('correct division when first argument has a decimal part less than .5', function () {
-      expect(calculateNumber('DIVIDE', 1.2, 3)).to.equal(1/3);
+      expect(calculateNumber('DIVIDE', 1.2, 3)).to.equal(1 / 3);
     });
   
     it('correct division when first argument has a decimal part greater than .5', function () {
-      expect(calculateNumber('DIVIDE', 1.6, 3)).to.equal(2/3);
+      expect(calculateNumber('DIVIDE', 1.6, 3)).to.equal(2 / 3);
     });
   
     it('correct division when second argument has a decimal part less than .5', function () {
-      expect(calculateNumber('DIVIDE', 1, 3.2)).to.equal(1/3);
+      expect(calculateNumber('DIVIDE', 1, 3.2)).to.equal(1 / 3);
     });
   
     it('correct division when second argument has a decimal part greater than .5', function () {
-      expect(calculateNumber('DIVIDE', 1, 3.6)).to.equal(1/4);
+      expect(calculateNumber('DIVIDE', 1, 3.6)).to.equal(1 / 4);
     });
   
     it('correct division when both arguments are decimals', function () {
-      expect(calculateNumber('DIVIDE', 1.2, 3.6)).to.equal(1/4);
+      expect(calculateNumber('DIVIDE', 1.2, 3.6)).to.equal(1 / 4);
     });
 
     it('correct division when second argument is 0', function () {
