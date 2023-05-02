2ecac0b2916313bf772172fa6868fa1e5cd662b7
hanichris <hanichris71@gmail.com>
2023-05-02 16:36:10 +0300

    using stub to test an expensive function call.

---
+++ 0x06-unittests_in_js/4-payment.test.js
@@ -0,0 +1,29 @@
+const { assert } = require('chai');
+const { describe, it } = require('mocha');
+const sinon = require('sinon');
+const sendPaymentRequestToApi = require('./3-payment');
+const Utils = require('./utils');
+
+describe('sendPaymentRequestToApi', function () {
+  let stubCalculateNumber,
+      spyConsoleLog;
+  beforeEach(function () {
+    stubCalculateNumber = sinon.stub(Utils, 'calculateNumber').callsFake(() => 42);
+    spyConsoleLog = sinon.spy(console, 'log');
+  });
+
+  afterEach(function () {
+    stubCalculateNumber.restore();
+    spyConsoleLog.restore();
+  });
+
+  it('should inspect call of expensive Utils function', function () {
+    sendPaymentRequestToApi(100, 20);
+    assert.isTrue(stubCalculateNumber.calledOnceWith('SUM', 100, 20));
+  });
+
+  it('should inspect the logging functionality', function () {
+    sendPaymentRequestToApi(100, 20);
+    assert.isTrue(spyConsoleLog.returned('The total is: 10'));
+  });
+});
