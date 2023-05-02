ad71bed4fc6249bcd44ec7067aa7fbf0cfaa5b54
hanichris <hanichris71@gmail.com>
2023-05-02 13:08:47 +0300

    test the `Utils.calculateNumber` method.

---
+++ 0x06-unittests_in_js/3-payment.test.js
@@ -0,0 +1,27 @@
+const { expect } = require('chai');
+const { describe, it } = require('mocha');
+const sinon = require('sinon');
+const sendPaymentRequestToApi = require('./3-payment');
+const Utils = require('./utils');
+
+describe('sendPaymentRequestToApi', function () {
+  var spyCalculateNumber;
+  beforeEach(function () {
+    spyCalculateNumber = sinon.spy(Utils, 'calculateNumber');
+  });
+
+  afterEach(function () {
+    spyCalculateNumber.restore();
+  });
+
+  it('should inspect call of the Utils function', function () {
+    sendPaymentRequestToApi(100, 20);
+    spyCalculateNumber.calledOnceWith(100, 20);
+  });
+
+  it('should inspect the math of the Utils function', function () {
+    const result = sendPaymentRequestToApi(100, 20);
+    expect(result).to.equal(spyCalculateNumber.returned(120));
+  });
+
+});
