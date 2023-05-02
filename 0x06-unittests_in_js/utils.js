03971f0f4692d7235217105c665a33c9a9d154db
hanichris <hanichris71@gmail.com>
2023-05-02 12:39:30 +0300

    Utils module defining the method `calculateNumber`
    `calculateNumber` takes 3 args: type, a and b. Based on the type argument, the method performs the appropriate operation.

---
+++ 0x06-unittests_in_js/utils.js
@@ -0,0 +1,22 @@
+class Utils {
+  calculateNumber(type, a, b) {
+    const [_a, _b] = [Math.round(a), Math.round(b)];
+    let result;
+    switch (type) {
+      case 'SUM':
+        result = _a + _b;
+        break;
+      case 'SUBTRACT':
+        result = _a - _b;
+        break;
+      case 'DIVIDE':
+        result = _b === 0 ? 'Error' : _a / _b;
+        break;
+      default:
+        break;
+    }
+    return result;
+  }
+}
+
+modules.exports = Utils;
