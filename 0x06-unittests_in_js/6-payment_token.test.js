3bb06187fc009b733e2cbcd576a6506c01f35458
hanichris <hanichris71@gmail.com>
2023-05-03 16:07:37 +0300

    use thenable technique to await promise execution

--- 0x06-unittests_in_js/6-payment_token.test.js
+++ 0x06-unittests_in_js/6-payment_token.test.js
@@ -1,13 +1,13 @@
+const { assert } = require('chai');
 const { describe, it } = require('mocha');
 const getPaymentTokenFromAPI = require('./6-payment_token');
 
 describe('getPaymentTokenFromAPI', function () {
   it('should test successful API call', function (done) {
-    getPaymentTokenFromAPI(true);
-    done();
-  });
-  it('should test an unsuccessful API call', function (done) {
-    getPaymentTokenFromAPI(false);
-    done();
+    getPaymentTokenFromAPI(true)
+      .then((data) => {
+        assert.deepEqual(data, { data: 'Successful response from the API' });
+        done();
+      });
   });
 });
